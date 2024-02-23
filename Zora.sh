feb66f04f3b321e92d79850a96e6f1bb9a1edbc0
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-02-24 00:44:41 +0200

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -3,8 +3,8 @@
 
 
 bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/TDM-Crypto.sh)
-sudo apt-get update && sudo apt-get upgrade -y
 
+sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
 
@@ -18,11 +18,10 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
-
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 
 git clone https://github.com/conduitxyz/node.git
-cd node
-./download-config.py zora-mainnet-0
+cd node || exit 1 
+./download-config.py zora-mainnet-0 || exit 1  
 export CONDUIT_NETWORK=zora-mainnet-0
-cp .env.example .env
+cp .env.example .env || exit 1 
