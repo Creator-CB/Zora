8d98f71afc9bba9ea01f0f083c72e3ced316f811
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-02-24 01:45:43 +0200

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/TDM-Crypto.sh)
 
-# Command 1
+# Function to check if a command executed successfully
+check_command() {
+    if [ $? -ne 0 ]; then
+        echo "Error executing command. Exiting..."
+        exit 1
+    fi
+}
+
+# Update and upgrade packages
 sudo apt-get update && sudo apt-get upgrade -y
+check_command
 
-# Command 2
+# Install necessary packages
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
+check_command
 
-# Command 3
+# Set up Docker repository
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Command 4
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+check_command
 
-# Command 5
+# Install Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
+check_command
 
-# Command 6
+# Clone repository and set up configuration
 git clone https://github.com/conduitxyz/node.git
-
-# Command 7
-cd node || exit 1  # Exit if directory change fails
-
-# Command 8
-./download-config.py zora-mainnet-0 || exit 1  # Exit if config download fails
-
-# Command 9
+check_command
+cd node
+./download-config.py zora-mainnet-0
+check_command
 export CONDUIT_NETWORK=zora-mainnet-0
-
-# Command 10
 cp .env.example .env
 
-# Open .env file for editing using nano
-nano .env
+echo "Installation completed successfully."
