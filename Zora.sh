9e35498bdee4c29e584f1f215c63984bdaf0bed7
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-02-24 00:17:33 +0200

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Update and upgrade the system
+
+bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/TDM-Crypto.sh)
 sudo apt-get update && sudo apt-get upgrade -y
 
-# Install necessary dependencies
+
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
 
-# Add Docker GPG key and repository
+
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Update package list with Docker repository
+
 sudo apt-get update
 
-# Install Docker
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
 
-# Clone the repository and perform necessary setup
+
 git clone https://github.com/conduitxyz/node.git
 cd node
 ./download-config.py zora-mainnet-0
