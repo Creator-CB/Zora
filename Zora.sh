c01ff597ed6b6d1e89267d8b6217e33746fd4669
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-03-03 03:58:12 +0200

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to check if a command executed successfully
 check_command() {
     if [ $? -ne 0 ]; then
         echo "Error executing command. Exiting..."
@@ -8,15 +7,12 @@ check_command() {
     fi
 }
 
-# Update and upgrade packages
 sudo apt-get update && sudo apt-get upgrade -y
 check_command
 
-# Install necessary packages
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
 check_command
 
-# Set up Docker repository
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -24,11 +20,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 check_command
 
-# Install Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 check_command
 
-# Clone repository and set up configuration
 git clone https://github.com/conduitxyz/node.git
 check_command
 cd node
